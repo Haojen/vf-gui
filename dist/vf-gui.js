6c5b7c97bde1832afa60911aba9265bd90cf114f
yangxiao <ifyx@qq.com>
2019-12-06 10:00:41 +0800

    ....

--- dist/vf-gui.js
+++ dist/vf-gui.js
@@ -9696,10 +9696,10 @@ const vfgui = __webpack_require__(/*! ./UI */ "./src/UI.ts");
 //     }
 // }
 // String.prototype.startsWith || (String.prototype.startsWith = function(word,pos?: number) {
-//     return this.lastIndexOf(word, pos0.7.9.0.7.9.0.7.9) ==0.7.9.0.7.9.0.7.9;
+//     return this.lastIndexOf(word, pos0.7.10.0.7.10.0.7.10) ==0.7.10.0.7.10.0.7.10;
 // });
 window.gui = vfgui;
-window.gui.version = "0.7.9";
+window.gui.version = "0.7.10";
 exports.default = vfgui;
 // declare namespace gui{
 //     export * from "src/UI";
